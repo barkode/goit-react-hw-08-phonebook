16ac8603b482a80a5b96f0650bdeec13ad37daf8
barkode <konstantinks@gmail.com>
2023-07-22 19:22:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { App } from 'components/App/App';
 import { store } from 'redux/store';
 import { Provider } from 'react-redux';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        {/* <BrowserRouter basename='/goit-react-hw-08-phonebook'> */}
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
